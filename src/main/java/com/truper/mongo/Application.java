815613f4794dcf81ca9e3c87449388524d2ac9e3
EduardoCM <EduardoCM>
2021-09-23 22:41:59 +0000

    Initial commit

---
+++ src/main/java/com/truper/mongo/Application.java
@@ -0,0 +1,10 @@
+package com.truper.mongo;
+
+import io.micronaut.runtime.Micronaut;
+
+public class Application {
+
+    public static void main(String[] args) {
+        Micronaut.run(Application.class, args);
+    }
+}
